726e3e4d8190b4723af6ee76d926f27a9afabb13
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-24 22:34:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-def my_each (arr)
-  counter = 0
-  while counter < arr.length
-    return arr[counter]
-    counter += 1
+def my_each(array)
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    array
   end
-  return arr
-end
